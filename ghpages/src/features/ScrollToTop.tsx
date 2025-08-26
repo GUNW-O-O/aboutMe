2603b1ca70434c6863c0bbd74144b95a03955ec6
GUNW-O-O <rhrjsdn12@naver.com>
2025-08-26 11:18:00 +0900

    위로가기버튼 만듬

--- ghpages/src/features/ScrollToTop.tsx
+++ ghpages/src/features/ScrollToTop.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop: React.FC = () => {
 
   const { pathname } = useLocation();
+
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     if ("scrollRestoration" in window.history) {
@@ -13,8 +15,50 @@ const ScrollToTop: React.FC = () => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 1000) { // 300px 이상 내려오면 버튼 표시
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
 
-  return null;
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // 부드럽게 스크롤
+    });
+  };
+
+
+  return (
+    <>
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          style={{
+            position: "fixed",
+            bottom: "5%",
+            right: "5%",
+            padding: "20px 20px",
+            borderRadius: "50%",
+            border: "none",
+            backgroundColor: "white",
+            color: "black",
+            cursor: "pointer",
+            zIndex: "1000",
+          }}
+        >
+          👆
+        </button>
+      )}
+    </>
+  );
 }
 
 export default ScrollToTop
